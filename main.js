7b6db13ea988b3b2840783a1d4a07ba9a89f08e8
JacobTyBruceLaptop <59230762+JacobTyBruce@users.noreply.github.com>
2022-06-24 13:38:04 -0500

    Grid compltely drawn and setup. Working on click event, not completely registering

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
 // main file for html display
 
-var canvas = document.getElementById('grid')
-var ctx = canvas.getContext('2d')
-ctx.fillStyle = 'red'
+// Grid Setup
 
-var map = []
+var canvas = document.getElementById("grid");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+
+var map = [];
 
 let slot_init = 0;
 
-let slot_width = 100;
-let slot_height = 100;
+let slot_width = 101;
+let slot_height = 101;
 let slot_num = 9;
 let gutter = 30;
 
-canvas.width = (slot_num*slot_width)+(gutter*slot_num)+gutter;
-canvas.height = (slot_num*slot_height)+(gutter*slot_num)+gutter;
+canvas.width = slot_num * slot_width + gutter * slot_num + gutter;
+canvas.height = slot_num * slot_height + gutter * slot_num + gutter;
 
 // loop to create map points
 
 // loop thru rows
-for (let r  = 0; r < slot_num; r++) {
-    // then by slots in the ind. row to allow for left-to-right dir in grid
-    for (let s = 0; s < slot_num; s++) {
-        // init top left corner
-        map[slot_init] = {
-            tl: {
-                x: s*slot_width+1+gutter*s+gutter,
-                y: r*slot_height+1+gutter*r+gutter,
-            },
-            content: null,
-        }
-        
-        /*
+for (let r = 0; r < slot_num; r++) {
+  // then by slots in the ind. row to allow for left-to-right dir in grid
+  for (let s = 0; s < slot_num; s++) {
+    // init top left corner
+    map[slot_init] = {
+      tl: {
+        x: s * slot_width + 1 + gutter * s + gutter,
+        y: r * slot_height + 1 + gutter * r + gutter,
+      },
+      content: null,
+    };
+
+    /*
         // remove gutter margin if applicable
         if (r == 0) {
             map[slot_init].tl.y -= gutter;
@@ -40,17 +42,75 @@ for (let r  = 0; r < slot_num; r++) {
             map[slot_init].tl.x -= gutter;
         }
         */
-        slot_init++;
-    }
+    slot_init++;
+  }
 }
 
-// draw dots in each tl corner
+// draw boxes around each grid slot
 for (let slot in map) {
-    ctx.beginPath();
-    ctx.moveTo(map[slot].tl.x, map[slot].tl.y);
-    //ctx.lineTo(map[slot].tl.x+slot_width/2, map[slot].tl.y+slot_height/2);
-    ctx.rect(map[slot].tl.x, map[slot].tl.y, slot_width, slot_height)
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(map[slot].tl.x, map[slot].tl.y);
+  //ctx.lineTo(map[slot].tl.x+slot_width/2, map[slot].tl.y+slot_height/2);
+  ctx.rect(map[slot].tl.x, map[slot].tl.y, slot_width, slot_height);
+  ctx.stroke();
 }
 
-console.log("Map Slots: ", map)
+console.log("Map Slots: ", map);
+
+// Slot Content Checker Interval
+setInterval(() => {
+  for (let s in map) {
+    if (map[s].content != null) {
+      ctx.beginPath();
+      ctx.arc(
+        map[s].tl.x + slot_width / 2 + 0.5,
+        map[s].tl.y + slot_height / 2 + 0.5,
+        slot_width / 2 - 10,
+        0,
+        2 * Math.PI
+      );
+      ctx.stroke();
+    }
+  }
+}, 100);
+
+// Canvas click event to check if slot clicked
+canvas.addEventListener(
+  "click",
+  (e) => {
+    console.log("-------Click Event-------");
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    console.log("x: " + x + " y: " + y);
+
+    // figure out what box click was in, if any
+
+    // first, divide click event position by padding+box length. This divides the grid into sectors.
+    // Using INTEGER division, you can find what box/sector the click was in by dividing the click event position by the padding+box length.
+    // Then, to find if the click event was in the padding or box region ofd the sector, use the MOD (%) of the posotion and box length + padding. This will return the reaminder i.e. how far in the sector it is, telling you if in paddong or in box
+
+    // Index : Position / Box Length+Padding
+    // Box or Padding : Position % Box Length+Padding
+
+    let xSector = Math.floor(x / (slot_height + gutter));
+    console.log("Click in x sector: " + xSector);
+
+    let ySector = Math.floor(y / (slot_width + gutter));
+    console.log("Click in y sector: " + ySector);
+
+    console.log("-------------------------");
+  },
+  false
+);
+
+// Create Map Button Function
+
+var createBtn = document.getElementById("create");
+
+createBtn.addEventListener("click", (e) => {
+  console.log("Create Clicked");
+  map[0].content = "test";
+  console.log(map[0]);
+});
